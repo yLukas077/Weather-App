1eac2957946765c0373fe5be8e38d606de66c551
yLukas077 <lukasmbs1234@gmail.com>
2024-04-11 23:06:51 -0300

    footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,2 +1,14 @@
-<footer class="content-info">
+<footer class="content-info bg-blue-400 text-white text-center pb-4 pt-4">
+  <div class="container mx-auto px-4">
+    <div class="flex justify-center items-center gap-8">
+      <a href="mailto:lukasmbs1234@gmail.com" class="flex items-center text-white no-underline hover:no-underline">
+        <i class="fas fa-envelope fa-lg mr-2"></i>
+        <span class="hover:underline">Contato: lukasmbs1234@gmail.com</span>
+      </a>
+      <a href="https://github.com/yLukas077/Weather-App" class="flex items-center text-white no-underline hover:no-underline">
+        <i class="fab fa-github fa-lg mr-2"></i>
+        <span class="hover:underline">Link do repositório</span>
+      </a>
+    </div>
+  </div>
 </footer>
